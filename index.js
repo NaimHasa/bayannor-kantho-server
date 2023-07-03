0ef4608466fe67975909ac163ec40e128ca8b199
Naim Hasan <naimarafat7@gmail.com>
2023-07-03 09:47:16 -0700

    loded data news

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 5000;
 
 
 const categories = require('./Data/Categrouies.json');
+const news = require('./Data/news.json');
 
 app.use(cors());
 
@@ -16,6 +17,28 @@ app.get('/news-categrouies', (req, res) => {
 
 app.get('/', (req, res) => {
     res.send('BayannorKantho New protal server side ')
+})
+
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '8') {
+        res.send(news);
+    }
+    else {
+        const category_news = news.filter(n => n.category_id === id)
+        res.send(category_news);
+    }
+
+})
+
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews);
+
+
 })
 
 app.listen(port, () => {
